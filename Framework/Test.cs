7e9400e79a09bb5d2429f15e10914336f5951d9f
Andrew Gardner <agardner@securenettech.com>
2020-01-13 08:25:05 -0700

    Did I exclude too much?

---
+++ Framework/Test.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace EchoPractice4Test.Framework
+{
+    abstract class TestInstruction
+    {
+        public abstract void Run();
+    }
+
+    class Test
+    {
+        List<TestInstruction> mInstructions;
+
+        Test(List<TestInstruction> l)
+        {
+            mInstructions = l;
+        }
+        ~Test()
+        {
+
+        }
+
+        void Run()
+        {
+            foreach (var instruction in mInstructions)
+            {
+                instruction.Run();
+            }
+        }
+    }
+}
---
+++ Tests/login.cs
@@ -0,0 +1,36 @@
+﻿using EchoPractice4Test.Framework;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+
+namespace EchoPractice4Test.Tests
+{
+    class Login : TestInstruction
+    {
+        State mState;
+        public Login(State state) 
+        {
+            mState = state;
+        }
+
+        override public void Run()
+        {
+            try
+            {
+                IWebElement link = mState.XPATH("/html/body/div[@id='app']/div[@class='v-dialog__content v-dialog__content--active']/div[@class='v-dialog v-dialog--active v-dialog--persistent']/div[@id='license_input_dialog']/div[@class='v-card__text']/div[@class='v-input v-text-field v-input--is-focused theme--light primary--text']/div[@class='v-input__control']/div[@class='v-input__slot']/div[@class='v-text-field__slot']/input[@id='email_address']");
+                link.Click();
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex);
+            }
+            finally
+            {
+                
+            }
+        }
+    }
+}
